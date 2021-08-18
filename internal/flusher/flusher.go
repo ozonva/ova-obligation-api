c2a11c184de5bc90f867022cdc84738a0acf41b7
Gregory L <gregoryleshkevich@gmail.com>
2021-08-18 18:03:27 +0300

    add flusher and repo

---
+++ internal/flusher/flusher.go
@@ -0,0 +1,33 @@
+package flusher
+
+import (
+	"github.com/ozonva/ova-obligation-api/internal/entity"
+	"github.com/ozonva/ova-obligation-api/internal/repo"
+)
+
+type Flusher interface {
+	Flush(entities []entity.Obligation) []entity.Obligation
+}
+
+func New(repository repo.Repo) Flusher {
+	return &flusher{repository: repository}
+}
+
+type flusher struct {
+	repository repo.Repo
+}
+
+func (f *flusher) Flush(entities []entity.Obligation) []entity.Obligation {
+	failed := make([]entity.Obligation, 0)
+	for _, entity := range entities {
+		if err := f.repository.AddEntitie(entity); err != nil {
+			failed = append(failed, entity)
+		}
+	}
+
+	if len(failed) > 0 {
+		return failed
+	}
+
+	return nil
+}
