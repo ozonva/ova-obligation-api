120184206aef6a03ea683602bc6640b33397dbb6
Gregory L <gregoryleshkevich@gmail.com>
2021-08-26 21:38:02 +0300

    Feature/task 6 (#6)

--- cmd/ova-obligation-api/main.go
+++ cmd/ova-obligation-api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"os"
+	"time"
+
+	"github.com/ozonva/ova-obligation-api/internal/server"
+	api "github.com/ozonva/ova-obligation-api/pkg/ova-obligation-api"
+	"github.com/rs/zerolog"
+	"google.golang.org/grpc"
+)
+
+const port = ":8080"
 
 func main() {
-	fmt.Println("The project description will be added later, since I don't know what is the project")
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	log := zerolog.New(output).With().Timestamp().Logger()
+
+	log.Info().Msgf("start serve port %s", port)
+
+	listen, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal().Msgf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	api.RegisterObligationRpcServer(s, server.NewObligationRpcServer(&log))
+	if err := s.Serve(listen); err != nil {
+		log.Fatal().Msgf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module github.com/ozonva/ova-obligation-api
 go 1.16
 
 require (
-	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/mock v1.6.0
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/onsi/ginkgo v1.16.4 // indirect
 	github.com/onsi/gomega v1.15.0
+	github.com/rs/zerolog v1.23.0
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210825212027-de86158e7fda // indirect
+	google.golang.org/grpc v1.40.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.27.1
 )
